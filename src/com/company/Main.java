fe1e296e338d832607939b41a8bbf4587942d486
ScotBlack <ScotBlack@users.noreply.github.com>
2020-12-09 15:45:13 +0100

    cruciale aanpassing

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myString = "Hello World!";
+        System.out.println(myString);
+        myString = myString + ", and Goodbye!";
+        System.out.println(myString);
+
+    }
+}
