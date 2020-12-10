cd7208a5d4790dfc59077df60e93e1a57f37f02f
ScotBlack <ScotBlack@users.noreply.github.com>
2020-12-10 13:51:21 +0100

    nieuwe aanpassing

---
+++ src/com/company/House.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class House {
+    private String streetName;
+    private String houseNumber;
+
+    //Constructor (1)
+    public House(String streetName, String houseNumber) {
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+    }
+
+    // Getters en Setters weggelaten
+
+    @Override
+    public String toString() {
+        return streetName + " " + houseNumber;
+    }
+}
