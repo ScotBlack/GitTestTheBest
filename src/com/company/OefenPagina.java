abb3df2aa33fad31e19d29d6a4bbf5509c0d71c9
ScotBlack <ScotBlack@users.noreply.github.com>
2020-12-10 14:27:08 +0100

    Pietje hoort er nu bij!

--- src/com/company/OefenPagina.java
+++ src/com/company/OefenPagina.java
@@ -1,15 +1,31 @@
 package com.company;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class OefenPagina {
     public static void main(String[] args) {
 
-        calcScore(true, 800, 5, 100);
-        calcScore(true, 5800, 127, 1080);
+    HashMap<String, Integer> playerPietje = new HashMap<>();
+        playerPietje.put("gameOver", 1);
+        playerPietje.put("score", 1000);
+        playerPietje.put("levelCompleted", 12);
+        playerPietje.put("bonus", 100);
+
+        int finalScorePietje = playerPietje.get("score");
+        System.out.println(finalScorePietje);
+
+
+
+        calcScore(1, 800, 5, 100);
+        calcScore(1, 5800, 127, 1080);
+
     }
 
-    public static void calcScore (boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static void calcScore (int gameOver, int score, int levelCompleted, int bonus) {
 
-        if (gameOver) {
+        if (gameOver == 1) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score = " + finalScore);
         }
