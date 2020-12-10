38f0878e0ded5f99184ebce3a0daf3830ccdf99f
ScotBlack <ScotBlack@users.noreply.github.com>
2020-12-10 16:43:02 +0100

    sjoerd doet ook weer mee

--- src/com/company/OefenPagina.java
+++ src/com/company/OefenPagina.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,23 +8,33 @@ import java.util.HashMap;
 public class OefenPagina {
     public static void main(String[] args) {
 
+    Integer[] henkie = new Integer[] {1, 300, 12, 200 };
+    System.out.println(henkie[1]);
+        
+    
     List<Integer> playerPietje = new ArrayList<>();
-        playerPietje.add(1);
-        playerPietje.add(1000);
-        playerPietje.add(12);
-        playerPietje.add(100);
+    playerPietje.add(1);
+    playerPietje.add(1000);
+    playerPietje.add(12);
+    playerPietje.add(100);
+    System.out.println(playerPietje);
+    calcScore(playerPietje.get(0), playerPietje.get(1), playerPietje.get(2), playerPietje.get(3));
+    
+    Integer[] sjoerd = addPlayer( 1, 20000, 5, 250);
+    
 
-        System.out.println(playerPietje);
-//        int finalScorePietje = calcScore(playerPietje);
-//        System.out.println(finalScorePietje);
+    calcScore(1, 800, 5, 100);
+    calcScore(1, 5800, 127, 1080);
 
-        calcScore(playerPietje.get(0), playerPietje.get(1), playerPietje.get(2), playerPietje.get(3));
+        
 
+        for (int i = 0; i < sjoerd.length; i++) {
+            System.out.println(sjoerd[i]);
+        }
+        calcScore(sjoerd[0], sjoerd[1], sjoerd[2], sjoerd[3] );
 
-        calcScore(1, 800, 5, 100);
-        calcScore(1, 5800, 127, 1080);
-    // test for git
     }
+
 
     public static void calcScore (int gameOver, int score, int levelCompleted, int bonus) {
 
@@ -32,4 +43,18 @@ public class OefenPagina {
             System.out.println("Your final score = " + finalScore);
         }
     }
+
+
+
+    public static Integer[] addPlayer (int gameOver, int score, int levelCompleted, int bonus) {
+       return new Integer[] {gameOver, score, levelCompleted, bonus};
+    }
+
+
+
+
+    Integer[] henkie = new Integer[] {1, 300, 12, 200 };
+
+
+
 }
